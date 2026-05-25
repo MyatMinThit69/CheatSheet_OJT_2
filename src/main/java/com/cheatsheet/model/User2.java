d0877042883b2ac24b75877bec66b4828f222841
myatminthit602 <thitminmyat5@gmail.com>
2026-05-25 14:11:48 +0630

    Create User2.java

---
+++ src/main/java/com/cheatsheet/model/User2.java
@@ -0,0 +1,15 @@
+package com.cheatsheet.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User2 {
+	private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+}
